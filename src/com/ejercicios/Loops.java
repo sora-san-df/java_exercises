20c1c738d8d1edd58f82be8ec14351c67144b338
cloud <cloudintheskyoutlook.es>
2023-01-12 09:22:43 -0500

    Loops exercise done

---
+++ src/com/ejercicios/Loops.java
@@ -0,0 +1,13 @@
+package com.ejercicios;
+
+public class Loops {
+
+    public static void main(String[] args) {
+        String[] nombres = new String[]{"Anette,camille,juan"};
+
+        for(String nombre: nombres){
+            System.out.println(nombre);
+        }
+    }
+
+}
